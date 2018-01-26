81598855a20cdb5eee2508999dd9894c7b5fdb21
claire <claire@localhost.localdomain>
2018-01-26 16:49:28 +0000

    Начальный проект

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Планировщик");
+
+            Console.WriteLine("Добавить");
+            Console.WriteLine("Удалить");
+            Console.WriteLine("Редактировать");
+        }
+    }
+}
